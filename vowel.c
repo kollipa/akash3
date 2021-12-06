ecc583f65c2c166e6697c47fa46ff80534fab550
kollipa <94820005+kollipa@users.noreply.github.com>
2021-12-06 19:29:36 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	char c;
+	scanf("%c",&c);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+	{
+		printf("char is a vowel");
+	}
+	else
+	{
+		printf("it is a consonant");
+	}
+}
