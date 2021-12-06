497aaad95ccf2a8b3cee4449310c0d32eb03bf3f
kollipa <94820005+kollipa@users.noreply.github.com>
2021-12-06 17:32:03 +0530

    Add files via upload

---
+++ divisible   by 5 and11.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%5==0&&a%11==0)
+	{
+		printf("it is divisible");
+	}
+	else
+	{
+		printf("it is not divisile");
+	}
+	
+}
---
+++ positive or negetive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("it is positive");
+	}
+	else
+	{
+		printf("it is negetive");
+	}
+}
